3081d9cf013f4f1ff8b60c990eabf00370c70e55
Vishnu <vishnuak15@gmail.com>
2021-10-11 16:32:59 +0530

    1st form done

--- pizza/views.py
+++ pizza/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 def home(request):
     return render(request,'pizza/home.html')
 
 def order(request):
-    return render(request,'pizza/order.html')
+    forms = PizzaForm()
+    return render(request,'pizza/order.html',{'Pizzaform':forms})
