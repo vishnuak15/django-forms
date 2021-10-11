8830242450c67dd97fa40b55d9e94d2fc2daf894
Vishnu <vishnuak15@gmail.com>
2021-10-11 16:55:22 +0530

    form done

--- pizza/views.py
+++ pizza/views.py
@@ -6,5 +6,15 @@ def home(request):
     return render(request,'pizza/home.html')
 
 def order(request):
-    forms = PizzaForm()
-    return render(request,'pizza/order.html',{'Pizzaform':forms})
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = " Thanks for ordering! Your %s %s and %s pizza is on its way!" % (filled_form.cleaned_data['size'],
+            filled_form.cleaned_data['topping1'],
+            filled_form.cleaned_data['topping2']
+            )
+            new_form = PizzaForm()
+            return render(request,'pizza/order.html',{'Pizzaform':new_form,'note':note})            
+    else:
+        forms = PizzaForm()
+        return render(request,'pizza/order.html',{'Pizzaform':forms})
